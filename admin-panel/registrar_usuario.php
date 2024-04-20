1ca86a13283c0eaec5d6d1ec6d955eb6a4389d30
Karmantial <98787733+Karmantial@users.noreply.github.com>
2024-04-20 09:50:52 -0600

    registro user

--- admin-panel/registrar_usuario.php
+++ admin-panel/registrar_usuario.php
@@ -1,4 +1,6 @@
 <?php
+header('Content-Type: application/json');
+
 $servername = "localhost";
 $username = "root"; // Cambiar por el nombre de usuario de tu base de datos
 $password = "admin"; // Cambiar por la contraseña de tu base de datos
@@ -7,13 +9,13 @@ $dbname = "mercado"; // Cambiar por el nombre de tu base de datos
 $conn = new mysqli($servername, $username, $password, $dbname);
 
 if ($conn->connect_error) {
-    die("Error en la conexión: " . $conn->connect_error);
+    die(json_encode(array("mensaje" => "Error en la conexión: " . $conn->connect_error)));
 }
 
 $stmt = $conn->prepare("CALL SP_REGISTRARUSUARIO(?, ?, ?, ?, ?, ?, @p_IdUsuarioResultado, @p_Mensaje)");
 
 if ($stmt === false) {
-    die("Error al preparar el procedimiento almacenado: " . $conn->error);
+    die(json_encode(array("mensaje" => "Error al preparar el procedimiento almacenado: " . $conn->error)));
 }
 
 $stmt->bind_param("ssssii", $_POST['codigo'], $_POST['nombre'], $_POST['correo'], $_POST['clave'], $_POST['rol'], $_POST['estado']);
@@ -28,11 +30,7 @@ if ($stmt->execute()) {
 
     echo json_encode($response);
 } else {
-    $response = array(
-        "mensaje" => "Error al ejecutar el procedimiento almacenado: " . $stmt->error
-    );
-
-    echo json_encode($response);
+    echo json_encode(array("mensaje" => "Error al ejecutar el procedimiento almacenado: " . $stmt->error));
 }
 
 $conn->close();
