aeb0dd2bbba45915f71c924d098ba955c0fc4a6a
Karmantial <karmantialuwu@gmail.com>
2024-04-19 23:06:34 -0600

    login.js

--- admin-panel/login.js
+++ admin-panel/login.js
@@ -1,15 +1,24 @@
 document.getElementById("loginForm").addEventListener("submit", function(event) {
-    event.preventDefault();
+    event.preventDefault(); // Evitar el envío predeterminado del formulario
+    
+    // Obtener los valores de usuario y contraseña del formulario
+    var username = document.getElementById("Cedula").value;
+    var password = document.getElementById("Clave").value;
 
-    var formData = new FormData(this);
-
+    // Realizar la solicitud POST al servidor para validar las credenciales
     fetch('login.php', {
         method: 'POST',
-        body: formData
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ username: username, password: password })
     })
     .then(response => response.json())
     .then(data => {
+        // Mostrar el mensaje de respuesta del servidor en el elemento con ID "mensaje"
         document.getElementById("mensaje").innerText = data.mensaje;
+        
+        // Redirigir a la página de inicio si el inicio de sesión es exitoso
         if (data.redireccionar) {
             window.location.href = data.redireccionar;
         }
